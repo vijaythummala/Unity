8a0a328d3b002402be3d1a20bfd9763f7c3241e6
VijayThummala <vijaytumalla@gmail.com>
2025-07-01 23:35:52 +0530

    updating vite config for react dependency visibility

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server : {
+  server: {
     port: 3000
+  },
+  build: {
+    rollupOptions: {
+      external: [],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        }
+      }
+    }
   }
 })
